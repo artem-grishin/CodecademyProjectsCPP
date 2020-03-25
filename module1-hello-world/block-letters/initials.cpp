69ddcf19ad84403c11a549069319259b60c4294a
cheehoo1 <artemgrishin@gmail.com>
2020-03-25 01:08:34 -0400

    first project

---
+++ module1-hello-world/block-letters/initials.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+//I like peanut butter and jelly
+int main(int argc, char *argv[]) {
+  cout << "  A  " << "   GGG " << endl;
+  cout << " A A " << "  G   G" << endl;
+  cout << "A   A" << "  G    " << endl;
+  cout << "AAAAA" << "  GGGGG" << endl;
+  cout << "A   A" << "  G   G" << endl;
+  cout << "A   A" << "  G   G" << endl;
+  cout << "A   A" << "   GGG " << endl;
+  return 0;
+}
